54c6edef89168d29d49c11a05fac13c552d6a57b
kght6123 <freelance@kght6123.jp>
2020-01-30 00:30:39 +0900

    レイアウト微調整

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="h-screen">
-    <div class="flex h-10 p-1">
+    <div class="flex h-10 py-1 px-2">
       <input class="shadow appearance-none border rounded w-full py-2 px-3 bg-gray-400 text-gray-900 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Title" v-model="title">
     </div>
-    <div class="flex h-10 p-1">
+    <div class="flex h-10 py-1 px-2">
       <input class="shadow appearance-none border rounded w-full py-2 px-3 bg-gray-400 text-gray-900 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Tags" v-model="tags">
     </div>
-    <div class="flex h-64 py-2 px-3">
-      <div class="w-1/2 bg-gray-400 h-full" v-html="$md.render(markdownText)"></div>
-      <textarea class="w-1/2 bg-gray-400 h-full" v-model="markdownText"></textarea>
+    <div class="flex h-64 py-1 px-2">
+      <div class="shadow appearance-none border rounded w-1/2 py-2 px-3 bg-gray-400 h-full" v-html="$md.render(markdownText)"></div>
+      <textarea class="shadow appearance-none border rounded w-1/2 py-2 px-3 bg-gray-400 h-full" v-model="markdownText"></textarea>
     </div>
-    <div class="flex items-center justify-between p-1">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline" type="button">
+    <div class="flex items-center justify-between py-1 px-2">
+      <button class="shadow appearance-none border rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline" type="button">
         投稿
       </button>
     </div>

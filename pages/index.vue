cba3508850ea1d6ca9c6b492d2206bb8ce24948b
kght6123 <freelance@kght6123.jp>
2020-02-11 23:47:13 +0900

    スタイルの共通化

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="h-screen">
     <div class="flex h-10 py-1 px-2">
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 bg-gray-400 text-gray-900 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Title" v-model="title">
+      <input class="w-full py-2 px-3 leading-tight" type="text" placeholder="Title" v-model="title">
     </div>
     <div class="flex h-10 py-1 px-2">
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 bg-gray-400 text-gray-900 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Tags" v-model="tags">
+      <input class="w-full py-2 px-3 leading-tight" type="text" placeholder="Tags" v-model="tags">
     </div>
     <div class="flex h-64 py-1 px-2">
-      <div class="shadow appearance-none border rounded w-1/2 py-2 px-3 bg-gray-400 h-full" v-html="$md.render(markdownText)"></div>
-      <textarea class="shadow appearance-none border rounded w-1/2 py-2 px-3 bg-gray-400 h-full" v-model="markdownText"></textarea>
+      <div class="w-1/2 py-2 px-3 h-full" v-html="$md.render(markdownText)"></div>
+      <textarea class="w-1/2 py-2 px-3 h-full" v-model="markdownText"></textarea>
     </div>
-    <div class="flex items-center justify-between py-1 px-2">
-      <button class="shadow appearance-none border rounded bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline" type="button">
-        投稿
+    <div class="flex items-center justify-between py-2 px-2">
+      <button class="py-2 px-4 w-full rounded" type="button" @click="registQuestion">
+        投稿する
+      </button>
+    </div>
+    <div class="flex items-center justify-between py-2 px-2">
+      <button class="py-2 px-4 w-full rounded" type="button" @click="searchQuestion">
+        検索する
       </button>
     </div>
   </div>
@@ -20,6 +25,10 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
+
+// FIXME:pluginでnuxtコンテキストに入れる
+const { Client } = require('elasticsearch')
+const client = new Client({ host: 'localhost:9200' })
 
 export default {
   components: {
@@ -30,6 +39,24 @@ export default {
       title: 'Hello World!!!',
       tags: 'hello world tag sample',
       markdownText: '# Hello MarkdownIt!!!\n```sh\n### Hello\necho "Hello!"\n```\n'
+    }
+  },
+  methods: {
+    async registQuestion() {
+      const res = await this.$axios.$post(
+        '/api/question/regist',
+        // `/api/test`,
+        { title: this.title, tags: this.tags, body: this.markdownText }
+      )
+      console.log(`regist complete!!!`, res)
+    },
+    async searchQuestion() {
+      const result = await client.search({
+        index: 'admin',
+        // body: { foo: 'bar' }
+        // _type	questions
+      })
+      console.log(`search complete!!!`, result)
     }
   }
 }

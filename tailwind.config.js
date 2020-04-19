c01d6f251f6ee8a40d9def187c68bacbb75c38fa
kght6123 <freelance@kght6123.jp>
2020-04-20 01:06:42 +0900

    レスポンシブフォントの設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,20 @@ module.exports = {
      '1/2': '50%',
      '3/4': '75%',
      'full': '100%',
-    }
+    },
+    // fontSize: {
+    //   'xs': '1vw',
+    //   'sm': '2vw',
+    //   'base': '3vw',
+    //   'lg': '4vw',
+    //   'xl': '5vw',
+    //   '2xl': '6vw',
+    //   '3xl': '7vw',
+    //   '4xl': '8vw',
+    //   '5xl': '9vw',
+    //   '6xl': '10vw',
+    //   '7xl': '11vw',
+    // }
   },
   variants: {},
   plugins: [
